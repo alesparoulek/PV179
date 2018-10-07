7f6c0784799c1c26e51f295604791a5494673848
Ales Paroulek <xparoul@fi.muni.cz>
2018-10-07 22:04:00 +0200

    WIP: added infrastructure

---
+++ PV179/JobsPortal.Infrastructure.EntityFramework/UnitOfWork/EntityFrameworkUnitOfWorkProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobsPortal.Infrastructure.UnitOfWork;
+using System.Data.Entity;
+
+namespace JobsPortal.Infrastructure.EntityFramework.UnitOfWork
+{
+    public class EntityFrameworkUnitOfWorkProvider : UnitOfWorkProviderBase
+    {
+        private readonly Func<DbContext> dbContextFactory;
+
+        public EntityFrameworkUnitOfWorkProvider(Func<DbContext> dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        public override IUnitOfWork Create()
+        {
+            UowLocalInstance.Value = new EntityFrameworkUnitOfWork(dbContextFactory);
+            return UowLocalInstance.Value;
+        }
+    }
+}
