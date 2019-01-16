4105718e5a7ff145560258780377c1bb272446e9
Ales Paroulek <xparoul@fi.muni.cz>
2019-01-16 22:40:29 +0100

    masked passwords + WIP: company registration

--- PV179/JobsPortal.BusinessLayer/DataTransferObjects/CompanyCreateDto.cs
+++ PV179/JobsPortal.BusinessLayer/DataTransferObjects/CompanyCreateDto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,29 @@ namespace JobsPortal.BusinessLayer.DataTransferObjects
 {
     public class CompanyCreateDto : DtoBase
     {
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Invalid name")]
+        [StringLength(64, ErrorMessage = "Maximum 64 characters exceeded")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("E-mail")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Login")]
+        [RegularExpression(@"^[a-zA-Z0-9_]+$", ErrorMessage = "Invalid login (only alphanumeric and _)")]
+        [StringLength(64, ErrorMessage = "Maximum 64 characters")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Password")]
+        [RegularExpression(@"^.{8,}$", ErrorMessage = "Minimum 8 characters required")]
+        [StringLength(64, ErrorMessage = "Maximum 64 characters")]
+        [PasswordPropertyText]
         public string Password { get; set; }
     }
 }
