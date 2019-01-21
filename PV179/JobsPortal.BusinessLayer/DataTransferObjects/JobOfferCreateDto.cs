ef81c0b320c701dbc8c85e1d2e6418b2b2016a4e
Ales Paroulek <xparoul@fi.muni.cz>
2019-01-21 01:24:45 +0100

    required fields

--- PV179/JobsPortal.BusinessLayer/DataTransferObjects/JobOfferCreateDto.cs
+++ PV179/JobsPortal.BusinessLayer/DataTransferObjects/JobOfferCreateDto.cs
@@ -14,12 +14,27 @@ namespace JobsPortal.BusinessLayer.DataTransferObjects
     {
         public Guid CompanyId { get; set; }
         public JobType JobType { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Name")]
+        [RegularExpression(@"^.{3,}$", ErrorMessage = "Minimum 3 characters required")]
+        [StringLength(64, ErrorMessage = "Maximum 64 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Description")]
+        [RegularExpression(@"^.{3,}$", ErrorMessage = "Minimum 3 characters required")]
+        [StringLength(1000, ErrorMessage = "Maximum 1000 characters")]
         public string Description { get; set; }
+
         public TimeJob TimeJob { get; set; }
         public Location Location { get; set; }
         public Education Education { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DisplayName("Salary")]
         public int Salary { get; set; }
+
         public string Questionnaire { get; set; }
         public DateTime Date { get; set; }
     }
